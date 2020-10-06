df299848a8ebe3e14e08c001eee91c552023f2cd
Moddit <moddit@mbp-van-spurt.home>
2020-10-06 20:51:47 +0200

    changes

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<?php wp_footer(); ?>
+<?php include('includes/panel.php')?>
+<?php growwwFooter()?>
+<?php growwwBranding();?>
+</body>
+</html>
---
+++ includes/navigation.php
@@ -0,0 +1,4 @@
+<div class="nav-bar">
+
+    <div class="js-nav-toggle nav-toggle"><?php _e('Menu', 'Boilerplate'); ?></span><span class="nav-toggle__icon"><span></span></span></div>
+</div>
