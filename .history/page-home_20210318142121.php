16f3d6fd8289252acd4db83661af39d30050c791
Kevin Brinkman <kevin@moddit.nl>
2021-06-04 11:05:12 +0200

    Update & Fixes

---
+++ .history/page-home_20210318142121.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Homepagina */ ?>
+<?php get_header(); ?>
+
+<section>
+	<div class="container">
+		<?php the_title(); ?>
+		<?php the_content(); ?>
+	</div>
+</section>
+
+<?php get_footer(); ?>
